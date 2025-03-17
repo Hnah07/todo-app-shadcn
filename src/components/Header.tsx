af80a960de84316c1a94950217c71039fe0a25b1
Hannah Casier <hannah.casier@me.com>
2025-03-17 12:22:20 +0100

    implemented themeswitcher with themeprovider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,38 @@
 import { Button } from "./ui/button";
-import { SunMedium } from "lucide-react";
+import { SunMedium, Moon } from "lucide-react";
+import { useTheme } from "./ThemeProvider";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
 
 const Header = () => {
+  const { setTheme, theme } = useTheme();
+
   return (
     <header className="flex w-full items-center justify-between">
       <h1 className="text-2xl font-bold">Todo App</h1>
-      <Button variant="outline" size="icon" className="cursor-pointer">
-        <SunMedium />
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon" className="cursor-pointer">
+            {theme === "dark" ? <Moon /> : <SunMedium />}
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 };
